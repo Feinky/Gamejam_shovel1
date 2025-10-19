571ad74155988dca8fa61489fa4b4e439356e7ae
Feinky <notsenio@gmail.com>
2025-10-19 18:34:38 +0800

    dead

--- Assets/script/GameTimer.cs
+++ Assets/script/GameTimer.cs
@@ -4,22 +4,29 @@ using UnityEngine.SceneManagement;
 
 public class GameTimer : MonoBehaviour
 {
+    public Transform target;
     public TMP_Text timerText;   // UI 顯示
     public float timeElapsed = 0f;
     public static float finalTime = 0f; // 用於 GameOver scene
 
-    private bool isGameOver = false;
+    public bool isGameEnd = false;
 
     void Update()
     {
-        if (!isGameOver)
+        if (target.position.y < -50)
+        {
+            dead();
+        }
+
+            if (!isGameEnd)
         {
             timeElapsed += Time.deltaTime;
-
             int minutes = Mathf.FloorToInt(timeElapsed / 60f);
             int seconds = Mathf.FloorToInt(timeElapsed % 60f);
             timerText.text = string.Format("{0:00}:{1:00}", minutes, seconds);
         }
+
+       
     }
 
     // 偵測碰到 Finish
@@ -33,16 +40,15 @@ public class GameTimer : MonoBehaviour
 
     void EndGame()
     {
-        isGameOver = true;
+        isGameEnd = true;
         finalTime = timeElapsed;
         SceneManager.LoadScene("GameEnd");
     }
 
-    private void dead(Transform transform)
+    private void dead()
     {
-        if (transform.position.y < 0)
-        {
+        
             SceneManager.LoadScene("GameOver");
-        }
+        
     }
 }
